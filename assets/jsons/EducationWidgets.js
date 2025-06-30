063a16fad1175aca3b0ae8c4c034cbb3cb9d6a57
Susan Yoon <awesomeyooner@gmail.com>
2025-06-29 23:59:20 -0700

    made welcome-hero universal

---
+++ assets/jsons/EducationWidgets.js
@@ -0,0 +1,9 @@
+const jsonData = 
+[
+    {
+        "header": "Brea Olinda High School",
+        "image": "assets/images/organizations/school_logo.png",
+        "link": "https://bohs.bousd.us/",
+        "description": "789 Wildcat Way </br> Class of 2025"
+    },
+]
